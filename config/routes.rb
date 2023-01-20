517fbf8d1f0056928e9fecd85c5906e2d70a033d
sheredega <sheredega303@gmail.com>
2023-01-20 15:41:02 +0200

    Enable messages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'login', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
 end
